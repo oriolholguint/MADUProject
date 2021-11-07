e43b055ce2da8ab6e5702dc5a237667531b49aa6
TruftedBug89 <39797722+TruftedBug89@users.noreply.github.com>
2021-11-07 10:34:42 +0100

    Form Genero

--- PruebaMenuMADU/PruebaMenuMADU/CrearGenero.Designer.cs
+++ PruebaMenuMADU/PruebaMenuMADU/CrearGenero.Designer.cs
@@ -29,12 +29,215 @@ namespace PruebaMenuMADU
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbImagen = new System.Windows.Forms.GroupBox();
+            this.txtUrlOculta = new System.Windows.Forms.TextBox();
+            this.btnCargarImagen = new System.Windows.Forms.Button();
+            this.pbImagenGenero = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BntCargarFondo = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtNombreGenero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbImagen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenGenero)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbImagen
+            // 
+            this.gbImagen.Controls.Add(this.txtUrlOculta);
+            this.gbImagen.Controls.Add(this.btnCargarImagen);
+            this.gbImagen.Controls.Add(this.pbImagenGenero);
+            this.gbImagen.Location = new System.Drawing.Point(55, 203);
+            this.gbImagen.Margin = new System.Windows.Forms.Padding(2);
+            this.gbImagen.Name = "gbImagen";
+            this.gbImagen.Padding = new System.Windows.Forms.Padding(2);
+            this.gbImagen.Size = new System.Drawing.Size(585, 149);
+            this.gbImagen.TabIndex = 8;
+            this.gbImagen.TabStop = false;
+            this.gbImagen.Text = "Imagen principal del Género";
+            // 
+            // txtUrlOculta
+            // 
+            this.txtUrlOculta.Location = new System.Drawing.Point(141, 26);
+            this.txtUrlOculta.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUrlOculta.Name = "txtUrlOculta";
+            this.txtUrlOculta.Size = new System.Drawing.Size(439, 20);
+            this.txtUrlOculta.TabIndex = 2;
+            // 
+            // btnCargarImagen
+            // 
+            this.btnCargarImagen.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargarImagen.Location = new System.Drawing.Point(141, 65);
+            this.btnCargarImagen.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCargarImagen.Name = "btnCargarImagen";
+            this.btnCargarImagen.Size = new System.Drawing.Size(158, 40);
+            this.btnCargarImagen.TabIndex = 1;
+            this.btnCargarImagen.Text = "Cargar Imagen";
+            this.btnCargarImagen.UseVisualStyleBackColor = true;
+            // 
+            // pbImagenGenero
+            // 
+            this.pbImagenGenero.Location = new System.Drawing.Point(4, 17);
+            this.pbImagenGenero.Margin = new System.Windows.Forms.Padding(2);
+            this.pbImagenGenero.Name = "pbImagenGenero";
+            this.pbImagenGenero.Size = new System.Drawing.Size(121, 104);
+            this.pbImagenGenero.TabIndex = 0;
+            this.pbImagenGenero.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.BntCargarFondo);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(55, 372);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(585, 149);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Imagen para el fondo del Género";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 26);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // BntCargarFondo
+            // 
+            this.BntCargarFondo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BntCargarFondo.Location = new System.Drawing.Point(152, 68);
+            this.BntCargarFondo.Margin = new System.Windows.Forms.Padding(2);
+            this.BntCargarFondo.Name = "BntCargarFondo";
+            this.BntCargarFondo.Size = new System.Drawing.Size(235, 40);
+            this.BntCargarFondo.TabIndex = 1;
+            this.BntCargarFondo.Text = "Elegir imagen de fondo";
+            this.BntCargarFondo.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 17);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 104);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Location = new System.Drawing.Point(55, 547);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(585, 149);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cargar musica de fondo para el Género";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 26);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(439, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(141, 65);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cargar Musica de fondo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(4, 17);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(121, 104);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtNombreGenero
+            // 
+            this.txtNombreGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreGenero.Location = new System.Drawing.Point(59, 71);
+            this.txtNombreGenero.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombreGenero.Multiline = true;
+            this.txtNombreGenero.Name = "txtNombreGenero";
+            this.txtNombreGenero.Size = new System.Drawing.Size(772, 84);
+            this.txtNombreGenero.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 25);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nombre del nuevo género :";
+            // 
+            // CrearGenero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1302, 843);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreGenero);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbImagen);
+            this.Name = "CrearGenero";
             this.Text = "CrearGenero";
+            this.gbImagen.ResumeLayout(false);
+            this.gbImagen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenGenero)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbImagen;
+        private System.Windows.Forms.TextBox txtUrlOculta;
+        private System.Windows.Forms.Button btnCargarImagen;
+        private System.Windows.Forms.PictureBox pbImagenGenero;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BntCargarFondo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtNombreGenero;
+        private System.Windows.Forms.Label label1;
     }
 }
