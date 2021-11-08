c36a766b4f1603ed000dad9f44fc992131f4cb4e
TruftedBug89 <39797722+TruftedBug89@users.noreply.github.com>
2021-11-08 11:25:12 +0100

    Diseño del form

--- PruebaMenuMADU/PruebaMenuMADU/CrearGenero.Designer.cs
+++ PruebaMenuMADU/PruebaMenuMADU/CrearGenero.Designer.cs
@@ -43,12 +43,15 @@ namespace PruebaMenuMADU
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.txtNombreGenero = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.gbImagen.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbImagenGenero)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbImagen
@@ -56,27 +59,27 @@ namespace PruebaMenuMADU
             this.gbImagen.Controls.Add(this.txtUrlOculta);
             this.gbImagen.Controls.Add(this.btnCargarImagen);
             this.gbImagen.Controls.Add(this.pbImagenGenero);
-            this.gbImagen.Location = new System.Drawing.Point(55, 203);
+            this.gbImagen.Location = new System.Drawing.Point(11, 203);
             this.gbImagen.Margin = new System.Windows.Forms.Padding(2);
             this.gbImagen.Name = "gbImagen";
             this.gbImagen.Padding = new System.Windows.Forms.Padding(2);
-            this.gbImagen.Size = new System.Drawing.Size(585, 149);
+            this.gbImagen.Size = new System.Drawing.Size(446, 372);
             this.gbImagen.TabIndex = 8;
             this.gbImagen.TabStop = false;
             this.gbImagen.Text = "Imagen principal del Género";
             // 
             // txtUrlOculta
             // 
-            this.txtUrlOculta.Location = new System.Drawing.Point(141, 26);
+            this.txtUrlOculta.Location = new System.Drawing.Point(40, 248);
             this.txtUrlOculta.Margin = new System.Windows.Forms.Padding(2);
             this.txtUrlOculta.Name = "txtUrlOculta";
-            this.txtUrlOculta.Size = new System.Drawing.Size(439, 20);
+            this.txtUrlOculta.Size = new System.Drawing.Size(364, 20);
             this.txtUrlOculta.TabIndex = 2;
             // 
             // btnCargarImagen
             // 
             this.btnCargarImagen.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCargarImagen.Location = new System.Drawing.Point(141, 65);
+            this.btnCargarImagen.Location = new System.Drawing.Point(171, 284);
             this.btnCargarImagen.Margin = new System.Windows.Forms.Padding(2);
             this.btnCargarImagen.Name = "btnCargarImagen";
             this.btnCargarImagen.Size = new System.Drawing.Size(158, 40);
@@ -86,10 +89,10 @@ namespace PruebaMenuMADU
             // 
             // pbImagenGenero
             // 
-            this.pbImagenGenero.Location = new System.Drawing.Point(4, 17);
+            this.pbImagenGenero.Location = new System.Drawing.Point(68, 17);
             this.pbImagenGenero.Margin = new System.Windows.Forms.Padding(2);
             this.pbImagenGenero.Name = "pbImagenGenero";
-            this.pbImagenGenero.Size = new System.Drawing.Size(121, 104);
+            this.pbImagenGenero.Size = new System.Drawing.Size(307, 209);
             this.pbImagenGenero.TabIndex = 0;
             this.pbImagenGenero.TabStop = false;
             // 
@@ -98,27 +101,27 @@ namespace PruebaMenuMADU
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.BntCargarFondo);
             this.groupBox1.Controls.Add(this.pictureBox1);
-            this.groupBox1.Location = new System.Drawing.Point(55, 372);
+            this.groupBox1.Location = new System.Drawing.Point(476, 203);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(585, 149);
+            this.groupBox1.Size = new System.Drawing.Size(402, 372);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Imagen para el fondo del Género";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(141, 26);
+            this.textBox1.Location = new System.Drawing.Point(15, 248);
             this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(439, 20);
+            this.textBox1.Size = new System.Drawing.Size(371, 20);
             this.textBox1.TabIndex = 2;
             // 
             // BntCargarFondo
             // 
             this.BntCargarFondo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BntCargarFondo.Location = new System.Drawing.Point(152, 68);
+            this.BntCargarFondo.Location = new System.Drawing.Point(93, 284);
             this.BntCargarFondo.Margin = new System.Windows.Forms.Padding(2);
             this.BntCargarFondo.Name = "BntCargarFondo";
             this.BntCargarFondo.Size = new System.Drawing.Size(235, 40);
@@ -128,10 +131,10 @@ namespace PruebaMenuMADU
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(4, 17);
+            this.pictureBox1.Location = new System.Drawing.Point(44, 26);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(121, 104);
+            this.pictureBox1.Size = new System.Drawing.Size(313, 200);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -140,27 +143,27 @@ namespace PruebaMenuMADU
             this.groupBox2.Controls.Add(this.textBox2);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.pictureBox2);
-            this.groupBox2.Location = new System.Drawing.Point(55, 547);
+            this.groupBox2.Location = new System.Drawing.Point(893, 203);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(585, 149);
+            this.groupBox2.Size = new System.Drawing.Size(398, 372);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Cargar musica de fondo para el Género";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(141, 26);
+            this.textBox2.Location = new System.Drawing.Point(13, 248);
             this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(439, 20);
+            this.textBox2.Size = new System.Drawing.Size(368, 20);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(141, 65);
+            this.button2.Location = new System.Drawing.Point(141, 284);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(158, 40);
@@ -170,10 +173,10 @@ namespace PruebaMenuMADU
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(4, 17);
+            this.pictureBox2.Location = new System.Drawing.Point(27, 26);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(121, 104);
+            this.pictureBox2.Size = new System.Drawing.Size(354, 200);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
@@ -190,12 +193,36 @@ namespace PruebaMenuMADU
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(25, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(248, 25);
             this.label1.TabIndex = 12;
             this.label1.Text = "Nombre del nuevo género :";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1302, 172);
+            this.panel1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(905, 71);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 84);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Crear Género";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // CrearGenero
             // 
@@ -207,6 +234,7 @@ namespace PruebaMenuMADU
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.gbImagen);
+            this.Controls.Add(this.panel1);
             this.Name = "CrearGenero";
             this.Text = "CrearGenero";
             this.gbImagen.ResumeLayout(false);
@@ -218,6 +246,7 @@ namespace PruebaMenuMADU
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -239,5 +268,7 @@ namespace PruebaMenuMADU
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox txtNombreGenero;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
