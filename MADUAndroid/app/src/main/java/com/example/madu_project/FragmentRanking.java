5b599324c4e02896c423060cb13367f9afb04e70
TruftedBug89 <39797722+TruftedBug89@users.noreply.github.com>
2021-11-26 09:10:36 +0100

    ranking first update

--- MADUAndroid/app/src/main/java/com/example/madu_project/FragmentRanking.java
+++ MADUAndroid/app/src/main/java/com/example/madu_project/FragmentRanking.java
@@ -8,10 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 
 public class FragmentRanking extends Fragment {
 
     View view;
+    MainActivity ma;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,7 +23,11 @@ public class FragmentRanking extends Fragment {
         view = inflater.inflate(R.layout.fragment_ranking, container, false);
 
         //Inicializar variables, Arrays, GridViews,... y metodos
+        TabLayout tl = view.findViewById(R.id.tablayoutranking);
+        this.ma = (MainActivity) getActivity();
+        for ( Genero : this.ma.generos) {
 
+        }
 
         return view;
     }
